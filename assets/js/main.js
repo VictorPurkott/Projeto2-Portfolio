9b627f3dfb1b54166973c22822bdf46fcec682a2
Victor Purkott <143750645+VictorPurkott@users.noreply.github.com>
2024-05-16 17:40:47 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+/* ==================== MIXITUP FILTER PORTFOLIO ==================== */
+
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* ==================== Link Active Work ==================== */
+
+const linkWork = document.querySelectorAll('.work__item')
+
+function activeWork() {
+    linkWork.forEach(l=> l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l=> l.addEventListener("click", activeWork))
+
+
+/* ==================== Work PopUp ==================== */
+
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+
+function portfolioItemDetails(portfolioItem) {
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portfolio__item-details").innerHTML;
+}
